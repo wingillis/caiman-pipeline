5ef922b603d72b548021f2f71f47879570442705
wingillis <win.gillis@gmail.com>
2018-07-25 00:57:47 -0400

    added more robust file renaming

--- caiman_pipeline/cli.py
+++ caiman_pipeline/cli.py
@@ -16,14 +16,15 @@ def cli():
 @click.option('--input-dir', '-i', default=os.getcwd(), type=click.Path(resolve_path=True, exists=True))
 @click.option('--downsample', '-d', default=4, type=int)
 def concat_tiffs(input_dir, downsample):
+    regex_pat = r'-[0-9]{3}\.tif$'
     # get tif files
     files = glob(os.path.join(input_dir, '*.tif'))
     # remove any files with 'concat' in the name - it means it's already concatenated
-    files = [f for f in files if 'concat' not in f]
+    files = [f for f in files if 'concat' not in os.path.basename(f)]
     processed = []
     for f in files:
         # append a 0 to the name if it doesn't have numbers
-        if len(f.split('-')) == 1:
+        if not re.findall(regex_pat, f):
             f2 = re.sub(r'\.tif$', '-000.tif', f)
             # rename the actual file - it's easier for everyone
             os.rename(f, f2)
@@ -31,7 +32,7 @@ def concat_tiffs(input_dir, downsample):
         processed += [f]
     processed = sorted(processed)
     # go through each file one-by-one and write to a larger tiffile
-    fname = processed[0].split('-')[0] + '-dnsmpl-{}x-concat.tif'.format(downsample)
+    fname = re.sub(regex_pat, '-dnsmpl-{}x-concat.tif'.format(downsample), processed[0])
     with tifffile.TiffWriter(fname, bigtiff=True) as out_file:
         for file in tqdm(processed, desc='Concatenating data'):
             # read in data frame by frame
