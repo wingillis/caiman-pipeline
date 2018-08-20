52c9d4664ee47f646ebe46132f0d83b9def4af6c
wingillis <win.gillis@gmail.com>
2018-08-20 16:58:06 -0400

    added an extra param to click argument

--- caiman_pipeline/cli.py
+++ caiman_pipeline/cli.py
@@ -82,7 +82,7 @@ def extract_pipeline(input_file, cnmf_options, out_file, n_procs):
 
 
 @cli.command(name='mc')
-@click.argument('input_file')
+@click.argument('input_file', type=click.Path(resolve_path=True))
 @click.option('--gsig', default=3, type=int)
 @click.option('--max-shifts', default=5, type=int)
 @click.option('--rigid-splits', default=50, type=int)
